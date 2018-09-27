70f9a12e01d054f5b91a8db928c6534df8ec983f
2018JTM2774 <shivam041994@gmail.com>
2018-09-27 14:39:18 +0530

    updated file

--- ps1.py
+++ ps1.py
@@ -1,3 +1,17 @@
-###### this is the first .py file ###########
-
-####### write your code here ##########
+"""python program to find valid crosses and dimension of crosses
+by Shivam Gupta
+Sept 27 2018"""
+print ("enter no. of columns and rows required")
+n = int(input())
+m = int(input())
+print (n," ",m)
+if n < 2 and n > 105:
+    print ("invalid range")
+elif m < 2 and m > 105:
+    print ("invalid range")
+else:
+    list = []
+    for i in range(n):
+        s = list(input())
+        list += (s)
+    print (list)
